c0fdd57cfd31066189683083d885388e23b43823
liuvasconcelos <ltenoriovasconcelos@gmail.com>
2019-05-01 14:04:17 -0300

    Initial Commit

---
+++ place-your-orderTests/place_your_orderTests.swift
@@ -0,0 +1,34 @@
+//
+//  place_your_orderTests.swift
+//  place-your-orderTests
+//
+//  Created by Livia Vasconcelos on 01/05/19.
+//  Copyright © 2019 Livia Vasconcelos. All rights reserved.
+//
+
+import XCTest
+@testable import place_your_order
+
+class place_your_orderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
